3d5c58c2bb2fba75abb996d81b582988798419ee
MatiasxVarela <matiasxvarela@gmail.com>
2024-05-03 20:54:14 -0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser';
 
@@ -20,6 +21,16 @@ async function bootstrap() {
   app.setGlobalPrefix('/api/v1');
   app.useLogger(new Logger());
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Api end points')
+    .setDescription('Bot api description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT);
   console.log(`Application is running on: ${process.env.PORT}`);
 }
